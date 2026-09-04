e2a40daba2962c606ecaa471d23f96174d3b9a51
jaehoo0305 <grooochyjeff1234@gmail.com>
2026-09-04 20:24:31 +0900

    close #35

--- week3/1. basic/04_bfs.py
+++ week3/1. basic/04_bfs.py
@@ -43,18 +43,25 @@ def bfs(graph, start):
     Returns:
         방문 순서 리스트
     """
-    visited = []
     
     # TODO: 큐 생성 및 시작 정점 추가
     ## 방문한 정점 집합
-    pass
+    
+    visited = [start]
+    queue = deque([start])
 
     # TODO: 큐가 빌 때까지 반복
     ## 큐에서 정점 꺼내기
     ## 인접한 정점들 확인
     ## 방문하지 않은 정점이면 큐에 추가
-    pass
-    
+
+    while len(queue) > 0:
+        cur = queue.popleft()
+        for neighbor in graph[cur]:
+            if neighbor not in visited:
+                queue.append(neighbor)
+                visited.append(neighbor)
+            
     return visited
 
 # 테스트 케이스
